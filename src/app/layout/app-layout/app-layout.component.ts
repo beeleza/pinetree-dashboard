9abaa418b66886badf5f5072901da0f030d889ce
beeleza <higorbeleza.dev@gmail.com>
2025-07-28 21:06:29 -0300

    removing angular material

--- src/app/layout/app-layout/app-layout.component.ts
+++ src/app/layout/app-layout/app-layout.component.ts
@@ -1,22 +1,10 @@
 import { AuthService } from './../../core/auth/auth.service';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [
-    RouterOutlet,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatListModule,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss',
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../core/auth/auth.service';
 import {
   FormControl,
@@ -11,14 +8,10 @@ import {
 } from '@angular/forms';
 import { LoginRequest } from '../../core/models/LoginRequest';
 import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
     ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
@@ -29,7 +22,6 @@ export class LoginComponent {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private snackBar: MatSnackBar
   ) {}
 
   loginForm = new FormGroup({
@@ -46,18 +38,17 @@ export class LoginComponent {
 
     this.authService.login(loginData).subscribe({
       next: (response) => {
-        this.snackBar.dismiss();
         this.router.navigate(['/dashboard']);
       },
       error: (err) => {
         const message =
           err.error?.message || 'Erro ao fazer login. Tente novamente.';
-        this.snackBar.open(message, 'Fechar', {
-          duration: 5000,
-          horizontalPosition: 'right',
-          verticalPosition: 'top',
-          panelClass: ['snack-error'], // opcional para estilizar
-        });
+        // this.snackBar.open(message, 'Fechar', {
+        //   duration: 5000,
+        //   horizontalPosition: 'right',
+        //   verticalPosition: 'top',
+        //   panelClass: ['snack-error'], // opcional para estilizar
+        // });
       }
     });
   }
